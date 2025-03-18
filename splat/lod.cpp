5c67031ea727bbd4bc864d73c1d5ac9ab180bf79
Benjamin Xia <benjx32@gmail.com>
2025-03-18 01:39:14 -0700

    C++ sort implementation

--- splat/lod.cpp
+++ splat/lod.cpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <cstring>
 #include <stdexcept>
+#include <algorithm>
 
 struct Vertex {
     float position[3];
@@ -15,52 +16,44 @@ struct Vertex {
     uint8_t rot[4];
 };
 
-std::vector<Vertex> processPlyBuffer(const std::vector<uint8_t>& buffer) {
-    const char* headerEnd = "end_header\n";
-    const char* data = reinterpret_cast<const char*>(buffer.data());
-    size_t headerEndPos = std::string(data, buffer.size()).find(headerEnd);
-    if (headerEndPos == std::string::npos) {
-        throw std::runtime_error("Unable to read .ply file header");
+std::vector<uint32_t> runSort(std::vector<float> viewProj,
+             std::vector<float> f_buffer,
+             size_t lastVertexCount,
+             size_t vertexCount) {
+    int maxDepth = -INT32_MAX;
+    int minDepth = INT32_MAX;
+    std::vector<uint32_t> depthIndex(vertexCount, 0);
+    std::vector<int32_t> sizeList(vertexCount);
+    for (size_t i = 0; i < vertexCount; i++) {
+        int depth =
+            static_cast<int>((viewProj[2] * f_buffer[8 * i + 0] +
+                viewProj[6] * f_buffer[8 * i + 1] +
+                viewProj[10] * f_buffer[8 * i + 2]) *
+                4096);
+        sizeList[i] = depth;
+        minDepth = std::min(minDepth, depth);
+        maxDepth = std::max(maxDepth, depth);
+    }
+    float depthInv = (256 * 256 - 1) / static_cast<float>(maxDepth - minDepth);
+    std::vector<size_t> counts0(256 * 256, 0);
+
+    for (size_t i = 0; i < vertexCount; i++) {
+        sizeList[i] = static_cast<int32_t>((sizeList[i] - minDepth) * depthInv);
+        counts0[sizeList[i]]++;
+    }
+    std::vector<uint32_t> starts0(256 * 256, 0);
+    for (size_t i = 1; i < 256 * 256; i++) {
+        starts0[i] = starts0[i - 1] + counts0[i - 1];
     }
 
-    size_t vertexCount = 0;
-    sscanf(data, "element vertex %zu", &vertexCount);
-
-    size_t dataOffset = headerEndPos + strlen(headerEnd);
-
-    std::vector<Vertex> vertices(vertexCount);
-
-    for (size_t i = 0; i < vertexCount; ++i) {
-        size_t offset = dataOffset + i * (3 * sizeof(float) + 3 * sizeof(float) + 4 + 4);
-
-        float x = *reinterpret_cast<const float*>(&buffer[offset]);
-        float y = *reinterpret_cast<const float*>(&buffer[offset + 4]);
-        float z = *reinterpret_cast<const float*>(&buffer[offset + 8]);
-
-        float scale_0 = *reinterpret_cast<const float*>(&buffer[offset + 12]);
-        float scale_1 = *reinterpret_cast<const float*>(&buffer[offset + 16]);
-        float scale_2 = *reinterpret_cast<const float*>(&buffer[offset + 20]);
-
-        uint8_t r = buffer[offset + 24];
-        uint8_t g = buffer[offset + 25];
-        uint8_t b = buffer[offset + 26];
-        uint8_t a = buffer[offset + 27];
-
-        uint8_t rot_0 = buffer[offset + 28];
-        uint8_t rot_1 = buffer[offset + 29];
-        uint8_t rot_2 = buffer[offset + 30];
-        uint8_t rot_3 = buffer[offset + 31];
-
-        vertices[i] = {
-            {x, y, z},
-            {std::exp(scale_0), std::exp(scale_1), std::exp(scale_2)},
-            {r, g, b, a},
-            {rot_0, rot_1, rot_2, rot_3}
-        };
+    for (size_t i = 0; i < vertexCount; i++) {
+        depthIndex[starts0[sizeList[i]]++] = i;
     }
+    return depthIndex;
+}
 
-    // TODO: Build tree from the vertices here
-    return vertices;
+std::vector<uint32_t> test2() {
+    return {69420};
 }
 
 std::string test() {
@@ -68,8 +61,12 @@ std::string test() {
 }
 
 EMSCRIPTEN_BINDINGS(wasm_gsplat) {
-    // emscripten::register_vector<uint8_t>("VectorUint8");
-    // emscripten::register_vector<Vertex>("VectorVertex");
-    emscripten::function("processPlyBuffer", &processPlyBuffer);
+    emscripten::register_vector<uint8_t>("VectorUint8");
+    emscripten::register_vector<Vertex>("VectorVertex");
+    emscripten::register_vector<uint32_t>("VectorUint32vector");
+    emscripten::register_vector<float>("VectorFloat32");
+    emscripten::function("runSort", &runSort);
+    // emscripten::function("processPlyBuffer", &processPlyBuffer);
     emscripten::function("test", &test);
+    emscripten::function("test2", &test2);
 }
